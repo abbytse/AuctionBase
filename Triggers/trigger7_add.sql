c384c017adeffb06860f88b10a5fcad6af885bc4
Abby Tse <abbytse7878@gmail.com>
2018-11-24 22:08:41 -0500

    new comments

--- Triggers/trigger7_add.sql
+++ Triggers/trigger7_add.sql
@@ -2,6 +2,7 @@
 PRAGMA foreign_keys = ON;
 DROP TRIGGER IF EXISTS time_flow;
 
+--Catch any instance of a time being inserted which is less than the previous time and ROLLBACK
 CREATE TRIGGER time_flow
 BEFORE UPDATE ON CurrentTime
 FOR EACH ROW
