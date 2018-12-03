379a540352d7c32527a40e70a34b6ac6ed5ce929
Abby Tse <abbytse7878@gmail.com>
2018-12-02 21:51:13 -0500

    command line prompt

--- commandLine.py
+++ commandLine.py
@@ -1,6 +1,7 @@
 #to parse command-line options and arguments
 
 import sys 
+import pandas as pd 
 
 arguments = []
 
@@ -10,3 +11,31 @@ for index in range(2, len(sys.argv)):
 
 for i in range(len(arguments)):
 	print (arguments[i])	
+
+#select * 
+#from toys 
+#where minPrice = 100 && maxPrice = 1000
+
+#connecting to the database 
+#connection = sqlite3.connect("auction.db")
+
+#cursor
+#crsr = connection.cursor()
+
+c = ''.join(arguments[2]); 
+
+arg1 = ''.join(arguments[1]);
+
+arg2 = ''.join(arguments[0]);
+
+s = 'select *'
+f = ' from ' + c
+w = ' where ' + arg1 + arg2 
+
+query = s + f + w 
+
+print (query) 
+
+#execute the statement 
+#crsr.execute(query)
+
